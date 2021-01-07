8ee428bfd1ac2c08be4f40b236f3dc567ace0f63
tin tran <duythien8399@gmail.com>
2021-01-07 17:21:23 +0700

    Them truy xuat cua cac Chuc Nang

---
+++ LTUDQL1_DOAN_NHOM10/BUS/TacGiaBUS.cs
@@ -0,0 +1,66 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class TacGiaBUS
+    {
+        public DataTable LayDanhSachTacGia()
+        {
+            try
+            {
+                TacGiaDAO TacGiaDAO = new TacGiaDAO();
+                return TacGiaDAO.LayDanhSachTacGia();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ThemTacGia(string tenTacGia)
+        {
+            try
+            {
+                TacGiaDAO TacGiaDAO = new TacGiaDAO();
+                DataTable dsTacGia = TacGiaDAO.LayDanhSachTacGia();
+                string maTacGia = "TG" + (int.Parse(dsTacGia.Rows[dsTacGia.Rows.Count - 1][0].ToString().Substring(2, 3)) + 1).ToString("000");
+                TacGiaDAO.ThemTacGia(maTacGia, tenTacGia);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void SuaTacGia(string maTacGia, string tenTacGia)
+        {
+            try
+            {
+                TacGiaDAO TacGiaDAO = new TacGiaDAO();
+                TacGiaDAO.SuaTacGia(maTacGia, tenTacGia);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void XoaTacGia(string maTacGia)
+        {
+            try
+            {
+                TacGiaDAO TacGiaDAO = new TacGiaDAO();
+                TacGiaDAO.XoaTacGia(maTacGia);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ LTUDQL1_DOAN_NHOM10/BUS/TheLoaiBUS.cs
@@ -0,0 +1,72 @@
+﻿using DAO.DAO.SachDAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class TheLoaiBUS
+    {
+        public DataTable LayDanhSachTheLoai()
+        {
+            try
+            {
+                TheLoaiDAO dsTheLoai = new TheLoaiDAO();
+                DataTable table = dsTheLoai.LayDanhSachTheLoai();
+                return table;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ThemTheLoai(string tenTheLoai)
+        {
+            try
+            {
+                TheLoaiDAO TheLoai = new TheLoaiDAO();
+                DataTable dsTheLoai = TheLoai.LayDanhSachTheLoai();
+                string maTheLoai = "TLS" + (int.Parse(dsTheLoai.Rows[dsTheLoai.Rows.Count - 1][0].ToString().Substring(3, 3)) + 1).ToString("000");
+                TheLoai.ThemTheLoai(maTheLoai, tenTheLoai);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void SuaTheLoai(string maTheLoai, string TenTheLoai)
+        {
+            try
+            {
+                TheLoaiDAO TheLoai = new TheLoaiDAO();
+                TheLoai.SuaTheLoai(maTheLoai, TenTheLoai);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void XoaTheLoai(string maTheLoai)
+        {
+            try
+            {
+                TheLoaiDAO TheLoai = new TheLoaiDAO();
+                TheLoai.XoaTheLoai(maTheLoai);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

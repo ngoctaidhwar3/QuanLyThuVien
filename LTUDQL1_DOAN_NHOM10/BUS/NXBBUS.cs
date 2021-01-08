7319c84c49d3ccc71b6c5ef4abb5c6323958726a
tin tran <duythien8399@gmail.com>
2021-01-08 11:08:41 +0700

    Them truy xuat cua cac Chuc Nang

---
+++ LTUDQL1_DOAN_NHOM10/BUS/NXBBUS.cs
@@ -0,0 +1,67 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class NXBBUS
+    {
+        public DataTable LayDanhSachNXB()
+        {
+            try
+            {
+                NXBDAO NXBDAO = new NXBDAO();
+                return NXBDAO.LayDanhSachNXB();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ThemNXB(string tenNXB)
+        {
+            try
+            {
+                NXBDAO NXBDAO = new NXBDAO();
+                DataTable dsNXB = NXBDAO.LayDanhSachNXB();
+                string maNXB = "NXB" + (int.Parse(dsNXB.Rows[dsNXB.Rows.Count - 1][0].ToString().Substring(3, 3)) + 1).ToString("000");
+                NXBDAO.ThemNXB(maNXB, tenNXB);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void SuaNXB(string maNXB, string tenNXB)
+        {
+            try
+            {
+                NXBDAO NXBDAO = new NXBDAO();
+                NXBDAO.SuaNXB(maNXB, tenNXB);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void XoaNXB(string maNXB)
+        {
+            try
+            {
+                NXBDAO NXBDAO = new NXBDAO();
+                NXBDAO.XoaNXB(maNXB);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ LTUDQL1_DOAN_NHOM10/BUS/SachBUS.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class SachBUS
+    {
+        public static bool WasExistBookId(string bookId)
+        {
+            return DAO.SachDAO.WasExistBookId(bookId);
+        }
+
+        public static DataTable GetDatasByCommand(string cmd)
+        {
+            return DAO.SachDAO.GetDatasByCommand(cmd);
+        }
+
+        public static DataTable GetDatasByStoredProcedure(string cmd)
+        {
+            return DAO.SachDAO.GetDatasByStoredProcedure(cmd);
+        }
+
+        public static int AddANewBook(DTO.Sach book)
+        {
+            return DAO.SachDAO.AddANewBook(book);
+        }
+
+        public static DataTable FindBook(DTO.Sach book)
+        {
+            return DAO.SachDAO.FindBook(book);
+        }
+    }
+}

7ceb444421e5a5ba2d8ff5d34593ea38f7ed83e1
tin tran <duythien8399@gmail.com>
2021-01-09 03:00:59 +0700

    Them truy xuat cua cac Chuc nang

---
+++ LTUDQL1_DOAN_NHOM10/BUS/PhieuThuTienPhatBUS.cs
@@ -0,0 +1,78 @@
+﻿using DAO;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class PhieuThuTienPhatBUS
+    {
+        public DataTable LayDSDocGiaNo()
+        {
+            try
+            {
+                PhieuThuTienPhatDAO dao = new PhieuThuTienPhatDAO();
+                return dao.LayDSDocGiaNo();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public int UpDatePhieuTra(PhieuThuTienPhatDTO pttp)
+        {
+            try
+            {
+                PhieuThuTienPhatDAO dao = new PhieuThuTienPhatDAO();
+                int nRow = dao.UpDatePhieuTra(pttp);
+                return nRow;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public DataTable LayMaPhieuthu()
+        {
+            try
+            {
+                PhieuThuTienPhatDAO dao = new PhieuThuTienPhatDAO();
+                return dao.LayMaPhieuThu();
+
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public int ThemPhieuThuTienPhat(PhieuThuTienPhatDTO pttp)
+        {
+            try
+            {
+                PhieuThuTienPhatDAO dao = new PhieuThuTienPhatDAO();
+                return dao.ThemPhieuThuTienPhat(pttp);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public DataTable DanhSachNhanVienThuQuy()
+        {
+            try
+            {
+                PhieuThuTienPhatDAO dao = new PhieuThuTienPhatDAO();
+                return dao.DanhSachNhanVienThuQuy();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ LTUDQL1_DOAN_NHOM10/BUS/QuanLyThanhLySach.cs
@@ -0,0 +1,161 @@
+﻿using DAO;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class QuanLyThanhLySachBUS
+    {
+        public DataTable LayThongTinSach(string MaSach)
+        {
+            try
+            {
+                QuanLyThanhLySachDAO quanLyThanhLySachDAO = new QuanLyThanhLySachDAO();
+                return quanLyThanhLySachDAO.LayThongTinSach(MaSach);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataTable LayDanhSachLyDoThanhLy()
+        {
+            try
+            {
+                QuanLyThanhLySachDAO quanLyThanhLySachDAO = new QuanLyThanhLySachDAO();
+                return quanLyThanhLySachDAO.LayDanhSachLyDoThanhLy();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataTable LayThongTinNhanVien(string MaNhanVien)
+        {
+            try
+            {
+                QuanLyThanhLySachDAO quanLyThanhLySachDAO = new QuanLyThanhLySachDAO();
+                return quanLyThanhLySachDAO.LayThongTinNhanVien(MaNhanVien);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string LayMaPhieuLonNhat()
+        {
+            try
+            {
+                QuanLyThanhLySachDAO quanLyThanhLySachDAO = new QuanLyThanhLySachDAO();
+                DataTable dtMaPhieuThanhLy = quanLyThanhLySachDAO.LayMaPhieuLonNhat();
+
+                string MaPhieuLonNhat = dtMaPhieuThanhLy.Rows[0]["MaPhieuThanhLy"].ToString();
+                int num = int.Parse(MaPhieuLonNhat.Substring(4)) + 1;
+                string MaMoi = "PTLS" + num.ToString("000");
+                return MaMoi;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int ThemPhieuThanhLySach(PhieuThanhLySachDTO phieuThanhLySach)
+        {
+            try
+            {
+                QuanLyThanhLySachDAO quanLyThanhLySachDAO = new QuanLyThanhLySachDAO();
+                int Row = quanLyThanhLySachDAO.ThemPhieuThanhLySach(phieuThanhLySach);
+
+                return Row;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int ThemChiTietPhieuThanhLySach(ChiTietPhieuThanhLySachDTO phieuChiTietThanhLySach)
+        {
+            try
+            {
+                QuanLyThanhLySachDAO quanLyThanhLySachDAO = new QuanLyThanhLySachDAO();
+                int Row = quanLyThanhLySachDAO.ThemChiTietPhieuThanhLySach(phieuChiTietThanhLySach);
+
+                return Row;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int XoaSach(string MaSach)
+        {
+            try
+            {
+                QuanLyThanhLySachDAO quanLyThanhLySachDAO = new QuanLyThanhLySachDAO();
+                int Row = quanLyThanhLySachDAO.XoaSach(MaSach);
+
+                return Row;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public void ThemQuanLyThanhLySach(string tenQuanLyThanhLySach)
+        {
+            try
+            {
+                QuanLyThanhLySachDAO QuanLyThanhLySach = new QuanLyThanhLySachDAO();
+                DataTable dsQuanLyThanhLySach = QuanLyThanhLySach.LayDanhSachLyDoThanhLy();
+                string maQuanLyThanhLySach = "LDTL" + (int.Parse(dsQuanLyThanhLySach.Rows[dsQuanLyThanhLySach.Rows.Count - 1][0].ToString().Substring(4, 3)) + 1).ToString("000");
+                QuanLyThanhLySach.ThemQuanLyThanhLySach(tenQuanLyThanhLySach, maQuanLyThanhLySach);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void SuaQuanLyThanhLySach(string tenQuanLyThanhLySach, string maQuanLyThanhLySach)
+        {
+            try
+            {
+                QuanLyThanhLySachDAO QuanLyThanhLySach = new QuanLyThanhLySachDAO();
+                QuanLyThanhLySach.SuaQuanLyThanhLySach(tenQuanLyThanhLySach, maQuanLyThanhLySach);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void XoaQuanLyThanhLySach(string maQuanLyThanhLySach)
+        {
+            try
+            {
+                QuanLyThanhLySachDAO QuanLyThanhLySach = new QuanLyThanhLySachDAO();
+                QuanLyThanhLySach.XoaQuanLyThanhLySach(maQuanLyThanhLySach);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

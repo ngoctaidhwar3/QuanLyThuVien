00bd54965b94bfaed65e1c9b1f9cc08776cbd1d0
tin tran <duythien8399@gmail.com>
2021-01-10 00:55:03 +0700

    Them file DocGia, NhanVien va cac chuc nang

---
+++ LTUDQL1_DOAN_NHOM10/BUS/DocGiaBUS/LoaiDocGiaBUS.cs
@@ -0,0 +1,74 @@
+﻿using DAO;
+using DAO.DocGiaDAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class LoaiDocGiaBUS
+    {
+        public DataTable LayDanhSachLoaiDocGia()
+        {
+            try
+            {
+                LoaiDocGiaDAO dsDocGia = new LoaiDocGiaDAO();
+                DataTable table = dsDocGia.LayDanhSachLoaiDocGia();
+                return table;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ThemLoaiDocGia(string tenLoaiDocGia)
+        {
+            try
+            {
+                LoaiDocGiaDAO LoaiDocGia = new LoaiDocGiaDAO();
+                DataTable dsLoaiDocGia = LoaiDocGia.LayDanhSachLoaiDocGia();
+                string maLoaiDocGia = "LDG" + (int.Parse(dsLoaiDocGia.Rows[dsLoaiDocGia.Rows.Count - 1][0].ToString().Substring(3, 1)) + 1).ToString("0");
+                LoaiDocGia.ThemLoaiDocGia(tenLoaiDocGia, maLoaiDocGia);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void SuaLoaiDocGia(string tenLoaiDocGia, string maLoaiDocGia)
+        {
+            try
+            {
+                LoaiDocGiaDAO LoaiDocGia = new LoaiDocGiaDAO();
+                LoaiDocGia.SuaLoaiDocGia(tenLoaiDocGia, maLoaiDocGia);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void XoaLoaiDocGia(string maLoaiDocGia)
+        {
+            try
+            {
+                LoaiDocGiaDAO LoaiDocGia = new LoaiDocGiaDAO();
+                LoaiDocGia.XoaLoaiDocGia(maLoaiDocGia);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
---
+++ LTUDQL1_DOAN_NHOM10/BUS/QuyDinhBUS.cs
@@ -0,0 +1,41 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class QuyDinhBUS
+    {
+        public DataTable LayDanhSachQuyDinh()
+        {
+            try
+            {
+                QuyDinhDAO dsBoPhan = new QuyDinhDAO();
+                DataTable table = dsBoPhan.LayDanhSachQuyDinh();
+                return table;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public void CapNhatQuyDinh(DataTable dtQuyDinh)
+        {
+            try
+            {
+                QuyDinhDAO QuyDinh = new QuyDinhDAO();
+                QuyDinh.CapNhatQuyDinh(dtQuyDinh);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

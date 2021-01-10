7eca7fb5650bf2d0c1e079a0af05acb1de555f7e
tin tran <duythien8399@gmail.com>
2021-01-10 13:11:33 +0700

    Them truy xuat trong file NhanVienBUS

---
+++ LTUDQL1_DOAN_NHOM10/BUS/NhanVienBUS/ChucVuBUS.cs
@@ -0,0 +1,73 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class ChucVuBUS
+    {
+        public DataTable LayDanhSachChucVu()
+        {
+            try
+            {
+                ChucVuDAO dsChucVu = new ChucVuDAO();
+                DataTable table = dsChucVu.LayDanhSachChucVu();
+                return table;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ThemChucVu(string tenChucVu)
+        {
+            try
+            {
+                ChucVuDAO ChucVu = new ChucVuDAO();
+                DataTable dsChucVu = ChucVu.LayDanhSachChucVu();
+                string maChucVu = "CV" + (int.Parse(dsChucVu.Rows[dsChucVu.Rows.Count - 1][0].ToString().Substring(2, 2)) + 1).ToString("00");
+                ChucVu.ThemChucVu(tenChucVu, maChucVu);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void SuaChucVu(string tenChucVu, string maChucVu)
+        {
+            try
+            {
+                ChucVuDAO ChucVu = new ChucVuDAO();
+                ChucVu.SuaChucVu(tenChucVu, maChucVu);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void XoaChucVu(string maChucVu)
+        {
+            try
+            {
+                ChucVuDAO ChucVu = new ChucVuDAO();
+                ChucVu.XoaChucVu(maChucVu);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
---
+++ LTUDQL1_DOAN_NHOM10/BUS/NhanVienBUS/NhanVienBUS.cs
@@ -0,0 +1,86 @@
+﻿using DAO;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class NhanVienBUS
+    {
+        public string LayMaNhanVienMoi()
+        {
+            try
+            {
+                NhanVienDAO dsNhanVien = new NhanVienDAO();
+                DataTable table = dsNhanVien.LayDanhSachNhanVien();
+                string MaCu = table.Rows[table.Rows.Count - 1].ItemArray[0].ToString();
+                int STT = int.Parse(MaCu.Substring(2, 4)) + 1;
+                string MaMoi = "NV" + STT.ToString("0000");
+                return MaMoi;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public DataTable LayDanhSachNhanVien()
+        {
+            try
+            {
+                NhanVienDAO dsNhanVien = new NhanVienDAO();
+                DataTable table = dsNhanVien.LayDanhSachNhanVien();
+                return table;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+        public DataTable LayDanhSachNhanVienTheoBoPhan(string maBoPhan)
+        {
+            try
+            {
+                NhanVienDAO dsNhanVien = new NhanVienDAO();
+                DataTable table = dsNhanVien.LayDanhSachNhanVienTheoBoPhan(maBoPhan);
+                return table;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ThemNhanVien(NhanVienDTO nhanVien)
+        {
+            try
+            {
+                NhanVienDAO NhanVien = new NhanVienDAO();
+                NhanVien.ThemNhanVien(nhanVien);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void CapNhatNhanVien(DataTable dtNhanVien)
+        {
+            try
+            {
+                NhanVienDAO NhanVien = new NhanVienDAO();
+                NhanVien.CapNhatNhanVien(dtNhanVien);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
